fbe281b0f5e8ae14dc70bd493bbec11899b7841c
swmac <shaynefw@gmail.com>
2023-01-30 18:16:18 -0500

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const input = document.querySelector(".inputBg");
+  input.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      window.open("https://www.google.com/search?q=celery&oq=cele&aqs=chrome.0.35i39j46i20i263i433i512j69i59j69i57j46i131i199i433i465i512j0i3j69i60l2.1190j0j7&sourceid=chrome&ie=UTF-8");
+    }
+  });
+
+  
